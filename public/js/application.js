8ad2b4ae18b5062c340c2e7111081bf2bcaea1c8
Apprentice <apprentice@devbootcamp.com>
2013-03-22 21:20:12 -0700

    added jQuery functions to survey creation page; attempted css on same page

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#update').submit(function(e) {
     e.preventDefault();
+
     var data = $(this).serializeArray();
     var survey = $(this).attr("class");
     // debugger
@@ -12,8 +13,25 @@ $(document).ready(function() {
     .done(function(data) {
       window.location = data;
     })
+  });
 
+  var lastQuestion = $('form div.question:last');
+  var counter = 2;
+
+  
+
+  $('#add-question').on('click', function() {
+    $(lastQuestion).clone(true).hide().insertBefore(lastQuestion).slideDown();
+    lastQuestion.attr('id', counter);
+    counter += 1;
+
+    $('div#'+counter+ ' input').focus();
     
+    $(function(){
+      $('body').animate({
+        scrollTop: $(lastQuestion).offset().top + 'px'
+      }, 'slow');
+    })
   });
 
 });
