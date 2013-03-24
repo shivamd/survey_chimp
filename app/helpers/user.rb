d8b2cfe7856a5a1c0078d1f84ff91023fb5fe14a
Shivam Daryanani <daryananis@gmail.com>
2013-03-24 10:43:45 -0700

    fixed login error messages, made a new error partial

--- app/helpers/user.rb
+++ app/helpers/user.rb
@@ -2,14 +2,13 @@ helpers do
 
   def login
     @user = User.find_by_email(params[:email])
-    if @user
-      if @user.password == params[:password]
+      if @user && @user.password == params[:password] 
         session[:id] = @user.id
         redirect '/users/profile'
       else
-        redirect '/users/login'
+        @errors = ["Invalid Password/Email combination"]
+        erb :login
       end
-    end
   end
 
   def current_user
