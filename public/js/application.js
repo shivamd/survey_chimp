41c76e27cd089f25d2ef33c12873f14cdf25c9df
Apprentice <apprentice@devbootcamp.com>
2013-03-24 17:34:56 -0700

    css on survey taking page, stats, and submitted page

--- public/js/application.js
+++ public/js/application.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
         scrollTop: $(lastQuestion).offset().top + 'px'
       }, 'slow');
     })
+
   });
 
   $('a.remove').on('click', function(e) {
@@ -41,12 +42,10 @@ $(document).ready(function() {
     });
   });
 
-
-
 // Bar Graph Stuff
 
 
-Array.prototype.randomElement = function () {
+  Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)]
   };
 
@@ -60,44 +59,44 @@ Array.prototype.randomElement = function () {
 
 // make graph go away when clicked again
 
-$('a.graph-button').on('click', function(e){
-  e.preventDefault();
-  var i = 0
-  var barColors = ["#4210AE", "#D5F800", "#8BA100", "#FFE4C4", '#8B0000', '#4B0082', '#FF6347']
-  var dataArray = new Array();
-  var choiceArray = $(this).parent().parent().find('li')
-  var choiceCount = $.each(choiceArray,function(index,value){ i += 1 });
-  var choiceList = $(this).parent().parent().children();
+  $('a.graph-button').on('click', function(e){
+    e.preventDefault();
+    var i = 0
+    var barColors = ["#4210AE", "#D5F800", "#8BA100", "#FFE4C4", '#8B0000', '#4B0082', '#FF6347']
+    var dataArray = new Array();
+    var choiceArray = $(this).parent().parent().find('li')
+    var choiceCount = $.each(choiceArray,function(index,value){ i += 1 });
+    var choiceList = $(this).parent().parent().children();
 
 
-  // choiceList.each(function(index, list){
-  //   dataArray.push( list.text().split(" ").join(",").split(",").reverse() );
-  // });
+    // choiceList.each(function(index, list){
+    //   dataArray.push( list.text().split(" ").join(",").split(",").reverse() );
+    // });
 
-  var rawData = $(this).parent().parent().children().children().text();
-  var data = rawData.split(" ");
+    var rawData = $(this).parent().parent().children().children().text();
+    var data = rawData.split(" ");
 
-  //jquery includes text in a href to the array. remove it
-  data.splice(data.length-3, 4);
-  
-  $.each(data, function(index,value){dataArray.push(value.split("-").join(",").split(",").reverse())})
+    //jquery includes text in a href to the array. remove it
+    data.splice(data.length-3, 4);
+    
+    $.each(data, function(index,value){dataArray.push(value.split("-").join(",").split(",").reverse())})
 
-  $.each(dataArray, function(index, value){
-    value.push(barColors.randomElement());
+    $.each(dataArray, function(index, value){
+      value.push(barColors.randomElement());
+    });
+
+    //append graph
+    var currentQuestion = $(this).attr("id");
+    $(".question_" + currentQuestion).jqBarGraph({ data: dataArray });
   });
 
-  //append graph
-  var currentQuestion = $(this).attr("id");
-  $(".question_" + currentQuestion).jqBarGraph({ data: dataArray });
-});
+
+  // $('#graphdiv').jqBarGraph({ data: arrayOfData }); 
 
 
-// $('#graphdiv').jqBarGraph({ data: arrayOfData }); 
+  // sorting questions
 
-
-// sorting questions
-
-$('form#update').sortable({handle: ".handle"}).disableSelection();
+  $('form#update').sortable({handle: ".handle"}).disableSelection();
 
 
 // dropdown menu
